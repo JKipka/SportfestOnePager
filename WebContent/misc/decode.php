31bdf3ebeea9f227e6735480cfd9e6ead1e95b71
kipka <jan.kipka@hpe.com>
2016-05-05 21:05:41 +0200

    Passwort Encrypter hinzugefügt, aufzurufen über http://localhost:8080/SportfestOnePager/misc/decode.php , das verschlüsselte Passwort so in DB einfügen

--- WebContent/misc/decode.php
+++ WebContent/misc/decode.php
@@ -1,29 +1,22 @@
+<!DOCTYPE HTML>
+<html>  
+<body>
+
+<form action="decode.php" method="post">
+Passwort: <input type="text" name="password"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
+
 <?php
 
-$str = "1234";
-$str = base64_encode($str);
-print $str."</br>";
 
-print base64_decode($str);
 
-print "</br>";
-
-$link = mysql_connect('localhost:3306', 'root', '');
-mysql_select_db('sportfest');
-if(!$link){
-	die("failed: ".mysql_error());
-}
-
-$newPW = "1234";
+$newPW = $_POST["password"];
 $newPW = base64_encode($newPW);
-$query = mysql_query("UPDATE admins SET password='$newPW' WHERE ID=1 AND password='asdf'");
 
-if(!$query){
-	die("failed: ".mysql_error());
-}else{
-	echo 'success';
-}
-
-mysql_close($link);
+print "Passwort verschlüsselt (so in DB einfügen): ".$newPW."</br>";
 
 ?>
