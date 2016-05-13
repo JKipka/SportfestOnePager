7110808868cba44984f789ee0f239be1c65206e6
kipka <jan.kipka@hpe.com>
2016-05-13 11:25:48 +0200

    Comments

--- WebContent/php/renewSession.php
+++ WebContent/php/renewSession.php
@@ -1,9 +1,14 @@
 <?php
 
+//Geloggten User ziehen
 $mail = $_POST["loggedUser"];
 
+
+//Cookie überschreiben
 setCookie( "user", $mail, time()+600, '/');
 
+
+//Weiterleiten
 header ( "Location: http://localhost:8080/SportfestOnePager/admin/adminHome.jsp?#editAdmins" );
 
 
